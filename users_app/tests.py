b7c44b96e3bcee107d5b4fd14fc1b8722a3e72a2
Bengt Früchtenicht <befru000@gmail.com>
2024-10-24 19:54:19 +0200

    feat: added get profile detail tests

--- users_app/tests.py
+++ users_app/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from .models import CustomerProfile, BusinessProfile
+from .api.serializers import CustomerProfileSerializer, BusinessProfileSerializer
 
-# Create your tests here.
+class ProfileTests(APITestCase):
+    
+    def setUp(self):
+        self.customer_user = User.objects.create_user(username="customeruser", password="customerpassword")
+        self.customer_profile = CustomerProfile.objects.create(user=self.customer_user)
+        self.business_user = User.objects.create_user(username="businessuser", password="businesspassword")
+        self.business_profile = BusinessProfile.objects.create(user=self.business_user, location="businesslocation", description="businessdescription")
+        self.client = APIClient()
+        
+    def test_get_profile_detail_customer(self):
+        url = reverse('profile-detail', kwargs={"pk": self.customer_user.id})
+        response = self.client.get(url)
+        expected_data = CustomerProfileSerializer(self.customer_profile).data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, expected_data)
+        
+    def test_get_profile_detail_business(self):
+        url = reverse('profile-detail', kwargs={"pk": self.business_user.id})
+        response = self.client.get(url)
+        expected_data =BusinessProfileSerializer(self.business_profile).data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, expected_data)
+        
