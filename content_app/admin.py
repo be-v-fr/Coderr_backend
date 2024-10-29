3f7cd613e90c2b4f542cd8658026afc17815eb28
Bengt Früchtenicht <befru000@gmail.com>
2024-10-29 10:28:01 +0100

    feat: added order and review models

--- content_app/admin.py
+++ content_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from content_app.models import Offer, OfferDetails 
+from content_app.models import Offer, OfferDetails, Order, CustomerReview
 
 admin.site.register(Offer)
 admin.site.register(OfferDetails)
+admin.site.register(Order)
+admin.site.register(CustomerReview)
--- content_app/models.py
+++ content_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.translation import gettext as _
 from users_app.models import BusinessProfile, CustomerProfile
 from content_app.utils import features_list_to_str
@@ -8,8 +8,8 @@ from datetime import date
 class Offer(models.Model):
     business_profile = models.ForeignKey(BusinessProfile, on_delete=models.CASCADE, related_name='business_profile_set')
     title = models.CharField(max_length=63, default=None, blank=True, null=True)
-    description = models.CharField(max_length=1024, default=None, blank=True, null=True)
-    image = models.CharField(max_length=64, default=None, blank=True, null=True)
+    description = models.CharField(max_length=1023, default=None, blank=True, null=True)
+    image = models.CharField(max_length=63, default=None, blank=True, null=True)
     created_at = models.DateField(default=date.today)
     updated_at = models.DateField(default=date.today)
     min_price = models.PositiveIntegerField(default=0, blank=True, null=True)
@@ -49,3 +49,38 @@ class OfferDetails(models.Model):
 
     def set_features_str(self, features_list):
         self.features = features_list_to_str(features_list)
+        
+class Order(models.Model):
+    IN_PROGRESS, COMPLETE, CANCELLED = 'in_progress', 'complete', 'cancelled'
+    STATUS_CHOICES = (
+            (IN_PROGRESS, _(IN_PROGRESS)),
+            (COMPLETE, _(COMPLETE)),
+            (CANCELLED, _(CANCELLED)),
+    )
+    status = models.CharField(
+        max_length=16,
+        choices=STATUS_CHOICES,
+        default=IN_PROGRESS,
+    )
+    orderer_profile = models.ForeignKey(CustomerProfile, on_delete=models.CASCADE, related_name='orders')
+    business_profile = models.ForeignKey(BusinessProfile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=63, default=None, blank=True, null=True)
+    offer_type = models.CharField(
+        max_length=16,
+        choices=OfferDetails.OFFER_TYPES,
+        default=OfferDetails.STANDARD,
+    )
+    created_at = models.DateField(default=date.today)
+    updated_at = models.DateField(default=date.today)
+    price = models.PositiveIntegerField(default=None, blank=True, null=True)
+    features = models.CharField(max_length=255, blank=True, default='')
+    revisions = models.PositiveIntegerField(validators=[MinValueValidator(-1)], default=None, blank=True, null=True)
+    delivery_time_in_days = models.PositiveIntegerField(default=None, blank=True, null=True)
+    
+class CustomerReview(models.Model):
+    reviewer_profile = models.ForeignKey(CustomerProfile, on_delete=models.CASCADE, related_name='reviews')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='reviews', default=None, blank=True, null=True)
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=None, blank=True, null=True)
+    description = models.CharField(max_length=1023, default=None, blank=True, null=True)
+    created_at = models.DateField(default=date.today)
+    updated_at = models.DateField(default=date.today)
