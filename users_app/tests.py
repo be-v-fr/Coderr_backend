26013a2f87e736bc596f3d32d8e1d1b584badac1
Bengt Früchtenicht <befru000@gmail.com>
2024-11-02 10:03:49 +0100

    refactor: added login logic to users tests

--- users_app/tests.py
+++ users_app/tests.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
 from .models import CustomerProfile, BusinessProfile
 from .api.serializers import CustomerProfileSerializer, BusinessProfileSerializer
 
@@ -10,8 +11,10 @@ class ProfileTests(APITestCase):
     def setUp(self):
         self.customer_user = User.objects.create_user(username="customeruser", password="customerpassword")
         self.customer_profile = CustomerProfile.objects.create(user=self.customer_user)
+        self.customer_token = Token.objects.create(user=self.customer_user)
         self.business_user = User.objects.create_user(username="businessuser", password="businesspassword")
         self.business_profile = BusinessProfile.objects.create(user=self.business_user, location="businesslocation", description="businessdescription")
+        self.business_token = Token.objects.create(user=self.business_user)
         self.client = APIClient()
         # add login logic after activating authentication
         
@@ -42,6 +45,7 @@ class ProfileTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         
     def test_patch_customer_profile_detail_ok(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.customer_token.key)
         new_username = 'customerpatch'
         data = {
             'user': {'username': new_username},
@@ -54,6 +58,7 @@ class ProfileTests(APITestCase):
         self.assertEqual(response.data, expected_data)
         
     def test_patch_business_profile_detail_ok(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.business_token.key)
         new_username = 'businesspatch'
         new_location = 'patchcity'
         data = {
@@ -69,6 +74,7 @@ class ProfileTests(APITestCase):
         self.assertEqual(response.data, expected_data)
         
     def test_patch_customer_profile_detail_bad_request(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.customer_token.key)
         new_username = 'customerpatch'
         data = {
             'user': new_username,
@@ -78,6 +84,7 @@ class ProfileTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         
     def test_patch_business_profile_detail_bad_request(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.business_token.key)
         new_username = 'businesspatch'
         data = {
             'user': new_username,
