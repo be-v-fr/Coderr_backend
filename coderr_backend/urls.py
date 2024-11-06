f54c053ca4c7693a1bd7a7ea7954e9757cc3e5d2
Bengt Früchtenicht <befru000@gmail.com>
2024-11-06 09:45:50 +0100

    refactor: created api url constant

--- coderr_backend/urls.py
+++ coderr_backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+API_BASE_URL = 'api/'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users_app.api.urls')),
-    path('api/', include('content_app.api.urls')),
-    path('api/', include('statistics_app.api.urls')),
+    path(API_BASE_URL, include('users_app.api.urls')),
+    path(API_BASE_URL, include('content_app.api.urls')),
+    path(API_BASE_URL, include('statistics_app.api.urls')),
 ]
