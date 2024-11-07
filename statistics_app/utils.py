ea644537641dc8730a0ac3a6f752612f46677e3e
Bengt Früchtenicht <befru000@gmail.com>
2024-11-07 10:51:05 +0100

    feat: user to profile access via related name

--- statistics_app/utils.py
+++ statistics_app/utils.py
@@ -4,8 +4,7 @@ from content_app.models import Order
 
 def get_business_user_orders(user_id):
     try:
-        user = User.objects.get(pk=user_id)
-        business_profile = BusinessProfile.objects.get(user=user)
-        return Order.objects.filter(business_profile=business_profile)
+        business_profile = User.objects.get(pk=user_id).business_profile
     except:
         return None
+    return Order.objects.filter(business_profile=business_profile)
--- users_app/models.py
+++ users_app/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+
+def get_user_field(related_name):
+    return models.OneToOneField(User, on_delete=models.CASCADE, related_name=related_name, blank=True, null=True)
     
 class AbstractUserProfile(models.Model):
     TYPE = None
-    class Meta:
-        abstract = True
     
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = get_user_field(related_name='+')
     created_at = models.DateField(default=datetime.date.today, blank=True, null=True)
     file = models.CharField(max_length=64, default=None, blank=True, null=True)
     uploaded_at = models.DateField(default=None, blank=True, null=True)
     
+    class Meta:
+        abstract = True
     
     def __str__(self):
         """
@@ -21,9 +24,11 @@ class AbstractUserProfile(models.Model):
     
 class CustomerProfile(AbstractUserProfile):
     TYPE = 'customer'
+    user = get_user_field(related_name='customer_profile')
     
 class BusinessProfile(AbstractUserProfile):
     TYPE = 'business'
+    user = get_user_field(related_name='business_profile')
     location = models.CharField(max_length=32, default=None, blank=True, null=True)
     description = models.CharField(max_length=1024, default=None, blank=True, null=True)
     working_hours = models.CharField(max_length=32, default=None, blank=True, null=True)
