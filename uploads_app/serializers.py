9049c212b7994f8f881e63e04040b2ddebcb39e9
Bengt Früchtenicht <befru000@gmail.com>
2024-11-14 19:41:20 +0100

    docs: uploads app comments

--- uploads_app/serializers.py
+++ uploads_app/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import FileUpload
 
 class FileUploadSerializer(serializers.ModelSerializer):
+    """
+    A serializer for the FileUpload model.
+    """
     class Meta:
         model = FileUpload
         fields = ['file', 'uploaded_at']
--- uploads_app/utils.py
+++ uploads_app/utils.py
@@ -1,6 +1,13 @@
 from .serializers import FileUploadSerializer
 
 def attach_file_to_obj(obj, file_data):
+    """
+    Attach a new file to an object, replacing the previous file if it exists.
+
+    Args:
+        obj (model instance): The object to which the file will be attached.
+        file_data (File): The file to attach to the object.
+    """
     try:
         serializer = FileUploadSerializer(data={'file': file_data})
         if serializer.is_valid(raise_exception=True):
@@ -14,5 +21,13 @@ def attach_file_to_obj(obj, file_data):
     return None
 
 def handle_file_update(obj, data_dict, file_key):
+    """
+    Check if a file update is requested and handle the file attachment.
+
+    Args:
+        obj (model instance): The object to which the file will be attached.
+        data_dict (dict): The dictionary containing the file data.
+        file_key (str): The key in the dictionary representing the file to be uploaded.
+    """
     if file_key in data_dict:
         return attach_file_to_obj(obj=obj, file_data=data_dict[file_key])
