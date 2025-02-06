4337753e6d93bde4fb975e5af0981d629127cfe6
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-02-06 15:01:27 +0100

    fix: missing slash

--- coderr_backend/wsgi.py
+++ coderr_backend/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coderr_backend.settings')
-os.environ.setdefault('FRONTEND_BASE_URL', 'https://coderr.bengt-fruechtenicht.de')
+os.environ.setdefault('FRONTEND_BASE_URL', 'https://coderr.bengt-fruechtenicht.de/')
 
 application = get_wsgi_application()
