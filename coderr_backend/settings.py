89bc8a53543e137ff953daab42ed2a1930615251
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-02-07 08:58:25 +0100

    fix: media url

--- coderr_backend/settings.py
+++ coderr_backend/settings.py
@@ -18,7 +18,7 @@ from secret_keys import DJANGO_SECRET_KEY, EMAIL_HOST, EMAIL_USER, EMAIL_PASSWOR
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
