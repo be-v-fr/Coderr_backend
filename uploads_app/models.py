39b9c491c63fa1baf3ee15fb75748670b3d8cd2d
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-02-07 08:54:38 +0100

    fix: upoad path

--- uploads_app/models.py
+++ uploads_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class FileUpload(models.Model):
-    file = models.FileField(upload_to='media/')
+    file = models.FileField(upload_to='')
     uploaded_at = models.DateTimeField(auto_now_add=True)
